682ab3379307c63583df77108da71d46a7cf56aa
Jose Vasquez <jous32@gmail.com>
2019-03-19 22:45:22 -0500

    fix cors :) and add seed dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Booking.create!([
+  {person_id: 1, function_id: 1},
+  {person_id: 1, function_id: 1},
+  {person_id: 1, function_id: 1},
+  {person_id: 1, function_id: 1}
+])
+Function.create!([
+  {displayed_at: "2019-01-01", movie_id: 1},
+  {displayed_at: "2019-01-02", movie_id: 1},
+  {displayed_at: "2019-01-03", movie_id: 1},
+  {displayed_at: "2019-01-04", movie_id: 2},
+  {displayed_at: "2019-01-05", movie_id: 2},
+  {displayed_at: "2019-01-06", movie_id: 3}
+])
+Movie.create!([
+  {name: "Movie 1", description: "A movie of terror", image_url: "http://www...."},
+  {name: "Movie 2", description: "A thriller movie", image_url: "http://www...."},
+  {name: "Movie 3", description: "A romance movie", image_url: "http://www...."}
+])
+Person.create!([
+  {name: "Peter", email: "peter@email.com", national_id: "1039"},
+  {name: "Ana", email: "ana@email.com", national_id: "1038"}
+])
